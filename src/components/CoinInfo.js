f04b3f62f8a6e7153c7243b81b585c86e8a67b0d
Awesomecoder26 <isaackhan21@gmail.com>
2022-01-12 17:42:18 +0000

    Created Historical Chart and Select Buttons

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,7 +1,142 @@
+import {
+  CircularProgress,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import { HistoricalChart } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { chartDays } from "../config/data";
+import SelectButton from "./SelectButton";
 
-const CoinInfo = () => {
-  return <div></div>;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const CoinInfo = ({ coin }) => {
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+
+  const { currency } = CryptoState();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+
+    setHistoricData(data.prices);
+  };
+  useEffect(() => {
+    fetchHistoricData();
+  }, [currency, days]);
+
+  const lightTheme = createTheme({
+    palette: {
+      main: "#fff",
+    },
+    type: "light",
+  });
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      width: "75%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 25,
+      padding: 40,
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+        marginTop: 0,
+        padding: 20,
+        paddingTop: 0,
+      },
+    },
+  }));
+
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <div className={classes.container}>
+        {!historicData ? (
+          <CircularProgress
+            style={{ color: "#26ddf9" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+
+                datasets: [
+                  {
+                    data: historicData.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} Days ) in ${currency}`,
+                    borderColor: "#26ddf9",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+
+            <div
+              style={{
+                display: "flex",
+                marginTop: 20,
+                justifyContent: "space-around",
+                width: "100%",
+              }}
+            >
+              {chartDays.map((day) => (
+                <SelectButton
+                  key={day.value}
+                  onClick={() => setDays(day.value)}
+                  selected={day.value === days}
+                >
+                  {day.label}
+                </SelectButton>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default CoinInfo;
---
+++ src/components/SelectButton.js
@@ -0,0 +1,33 @@
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+
+const SelectButton = ({ children, selected, onClick }) => {
+  const useStyles = makeStyles({
+    selectbutton: {
+      border: "1px solid #26ddf9 ",
+      borderRadius: 5,
+      padding: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontFamily: "Montserrat",
+      cursor: "pointer",
+      backgroundColor: selected ? "#26ddf9" : "",
+      color: selected ? "white" : "",
+      fontWeight: selected ? 700 : 500,
+      "&:hover": {
+        backgroundColor: "#26ddf9",
+        color: "white",
+      },
+      width: "22%",
+    },
+  });
+
+  const classes = useStyles();
+  return (
+    <span onClick={onClick} className={classes.selectbutton}>
+      {children}
+    </span>
+  );
+};
+
+export default SelectButton;
